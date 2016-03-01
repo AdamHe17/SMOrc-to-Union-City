e20ccc70833f6552792c7d783b2ceef3625a5b7a
TheOssumOpossum <v.bommier@gmail.com>
2016-03-01 11:38:21 -0600

    minor changes, still can't get the scene to work

--- DataScript.cs
+++ DataScript.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System;
-using UnityEngine.SceneManagement;
+
 
 public class DataScript : MonoBehaviour {
-
-    public static int p1hp = 100;
-    public static int p2hp;
-    public static int p3hp;
-    public static int supply = 10;
-
+	
+	public static int p1hp = 100;
+	public static int p2hp;
+	public static int p3hp;
+	public static int supply = 10;
+	
 }
