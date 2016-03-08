8a8c6c7befca513cbecb208f66dbbad4f1898c0f
Adam He <shuaihe2018@u.northwestern.edu>
2016-03-08 15:11:03 -0600

    added numeric support for event selection

--- ExploreAssets/ExploreBuildingScript.cs
+++ ExploreAssets/ExploreBuildingScript.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 using System;
+using UnityEngine.EventSystems;
 
 public class ExploreBuildingScript: ExploreScript {
 
@@ -14,7 +15,22 @@ public class ExploreBuildingScript: ExploreScript {
         }
     }
     void Update() {
-
+        PointerEventData pointer = new PointerEventData(EventSystem.current);
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            ExecuteEvents.Execute(event1.gameObject, pointer, ExecuteEvents.submitHandler);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            ExecuteEvents.Execute(event2.gameObject, pointer, ExecuteEvents.submitHandler);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3)) {
+            ExecuteEvents.Execute(event3.gameObject, pointer, ExecuteEvents.submitHandler);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            ExecuteEvents.Execute(event4.gameObject, pointer, ExecuteEvents.submitHandler);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha5)) {
+            ExecuteEvents.Execute(event5.gameObject, pointer, ExecuteEvents.submitHandler);
+        }
     }
 
     void ExploreBuilding() {
